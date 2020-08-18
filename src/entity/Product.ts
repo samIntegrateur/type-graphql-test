49cf14ef515eb4de87c0e4dec559a5a49a2a4122
Samuel Desbos <samuel.desbos@gmail.com>
2020-08-18 17:58:46 +0200

    Create a "higher order" resolver

---
+++ src/entity/Product.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { Field, ID, ObjectType } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class Product extends BaseEntity {
+
+    @Field(() => ID)
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Field()
+    @Column()
+    name: string;
+}
